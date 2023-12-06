d621a75f68b98cb608f476eef358af304b2107ff
jyee222 <152760280+jyee222@users.noreply.github.com>
2023-12-06 19:52:28 +0900

    Update weight-on-the-moon.java

--- 231206/달에서 무게 구하기/weight-on-the-moon.java
+++ 231206/달에서 무게 구하기/weight-on-the-moon.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        double a = 13;
+        double a = 13;                                       // int a = 13;
         double b = 0.165;
 
-        double c = a * b;
+        double c = a * b;                                    // 삭제
 
-        System.out.printf("%.0f * %.6f = %.6f", a, b, c);
+        System.out.printf("%.0f * %.6f = %.6f", a, b, c);    // ("%d * %.6f = %.6f", a, b, a * b)
     }
 }
