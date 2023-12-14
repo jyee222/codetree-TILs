56b3e236ed2cf1a6681cf91f1e07ea254677d942
jyee222 <152760280+jyee222@users.noreply.github.com>
2023-12-14 21:02:53 +0900

    Update replacing-variable-values-3.java

--- 231214/변수 값 교체하기 3/replacing-variable-values-3.java
+++ 231214/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -11,4 +11,16 @@ public class Main {
         System.out.println(a);
         System.out.println(b);
     }
+
+    // 변수 선언
+    // int a = 3, b = 5;
+
+    // 교체
+    // int temp = a;
+    // a = b;
+    // b = temp;
+
+    // 출력
+    // System.out.println(a);
+    // System.out.println(b);
 }
