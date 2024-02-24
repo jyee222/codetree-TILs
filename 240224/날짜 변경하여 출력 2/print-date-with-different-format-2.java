78d22417001a6862dec4bbaeeb248871388dd7e4
jyee222 <152760280+jyee222@users.noreply.github.com>
2024-02-24 23:53:41 +0900

    Update print-date-with-different-format-2.java

--- 240224/날짜 변경하여 출력 2/print-date-with-different-format-2.java
+++ 240224/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -13,3 +13,14 @@ public class Main {
         System.out.println(year + "." + month + "." + day);
     }
 }
+
+
+        // 변수 선언 및 입력
+        // Scanner sc = new Scanner(System.in);
+        // sc.useDelimiter("-"); // - 기준으로 잘라서 입력 받겠다는 뜻
+        // int m = sc.nextInt(); // - 기호가 나오기 전까지 입력 진행
+        // int d = sc.nextInt(); // - 기호가 나오기 전까지 입력 진행
+        // int y = sc.nextInt(); // - 기호가 나오기 전 or 입력의 마지막 부분까지 진행
+
+        // 출력
+        // System.out.println(y + "." + m + "." + d);
